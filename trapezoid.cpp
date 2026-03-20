92926fae16740e6cdb586a4a8acdaa898d025207
Polina Kiselyova <polinakiseleva307@gmail.com>
2026-03-20 12:41:44 +0700

    new

--- trapezoid.cpp
+++ trapezoid.cpp
@@ -18,17 +18,21 @@ int main() {
     cout << "Высота h: ";
     cin >> h;
     
-    
     if (a <= 0 || b <= 0 || c <= 0 || d <= 0 || h <= 0) {
         cout << "Все значения должны быть положительными" << endl;
         return 1;
     }
-    
+
     if ((a + b + c < d) || (a + b + d < c) || (a + c + d < b) || (c + b + d < a)){
         cout << "Сумма трёх любых сторон должна быть больше четвёртой" << endl;
         return 1;
     }
     
+    if (h > c || h > d){
+	cout << "Высота должны быть меньше боковых сторон" << endl;
+	return 1;
+    }
+
     double P = a + b + c + d;
     double S = ((a + b) / 2) * h;
     double M = (a + b) / 2;
@@ -36,6 +40,6 @@ int main() {
     cout << "Периметр трапеции: " << P << endl;
     cout << "Площадь трапеции: " << S << endl;
     cout << "Средняя линия: " << M << endl;
-    
+
     return 0;
 }
